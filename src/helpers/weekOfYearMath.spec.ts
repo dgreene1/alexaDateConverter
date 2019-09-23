0670200c2a401937b50155479eced3ddecc999a1
Dan Greene <14020024+dgreene1@users.noreply.github.com>
2019-09-23 17:41:22 -0400

    coverage for the helpers

--- src/helpers/weekOfYearMath.spec.ts
+++ src/helpers/weekOfYearMath.spec.ts
@@ -1,32 +1,52 @@
-import { LocalDate, DayOfWeek } from "js-joda"
-import { daysDelta } from "./weekOfYearMath";
+import { assertUnreachable, doIfNever } from "./neverCheckers";
 
-describe('daysDelta', ()=>{
+type AStringUnion = "a" | "b";
 
-    it('should produce 0 for Thursday to Thursday', ()=> {
+describe('assertUnreachable', ()=>{
+
+    it('should throw if the union has been narrowed to the point of never', ()=> {
         // Arrange
-        const firstThursdayOfJan2009 = LocalDate.of(2009,1,1);
-        // red-herring check
-        expect(firstThursdayOfJan2009.dayOfWeek()).toEqual(DayOfWeek.THURSDAY);
-        const sameDayOfWeek = DayOfWeek.THURSDAY;
+        const aValueThatShouldNeverHappen: AStringUnion = "c" as any;
 
         // Act
-        const result = daysDelta(firstThursdayOfJan2009, sameDayOfWeek);
+        let exceptionToTest: Error | null = null;
+        try {
+            if(aValueThatShouldNeverHappen === "a") {
+                expect("we shouldn't get here").toBeFalsy();
+            } else if (aValueThatShouldNeverHappen === "b") {
+                expect("we shouldn't get here").toBeFalsy();
+            } else {
+                assertUnreachable(aValueThatShouldNeverHappen);
+            }
+        } catch(err){
+            exceptionToTest = err;
+        }
 
         // Assert
-        expect(result).toEqual(0);
+        expect(exceptionToTest).toEqual(new Error(`Unhandled case: undefined --> "c"`));
     })
-    it('should produce 0 for Thursday to Saturday', ()=> {
+})
+
+
+describe('doIfNever', ()=>{
+
+    it('should return the result of the callback if the union has been narrowed to the point of never', ()=> {
         // Arrange
-        const firstThursdayOfJan2009 = LocalDate.of(2009,1,1);
-        // red-herring check
-        expect(firstThursdayOfJan2009.dayOfWeek()).toEqual(DayOfWeek.THURSDAY);
-        const twoDaysLater = DayOfWeek.SATURDAY;
+        const aValueThatShouldNeverHappen: AStringUnion = "c" as any;
+        const valueToExpect = `let's default to "a"`;
+        const callbackToDo = ()=> valueToExpect;
 
         // Act
-        const result = daysDelta(firstThursdayOfJan2009, twoDaysLater);
+        let result: string = "uninitialized";
+        if(aValueThatShouldNeverHappen === "a") {
+            result = "a";
+        } else if (aValueThatShouldNeverHappen === "b") {
+            result = "b";
+        } else {
+            result = doIfNever(aValueThatShouldNeverHappen, callbackToDo);
+        }
 
         // Assert
-        expect(result).toEqual(2);
+        expect(result).toEqual(valueToExpect);
     })
 })
